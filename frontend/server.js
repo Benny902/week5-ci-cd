381e6c415310a2c63862afd56b7e88fa4e5faf3a
Benny <Benny902@gmail.com>
2025-06-04 12:54:50 +0300

    fix lint

--- frontend/server.js
+++ frontend/server.js
@@ -1,4 +1,10 @@
-const express = require('express');
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const port = 4000;
 
